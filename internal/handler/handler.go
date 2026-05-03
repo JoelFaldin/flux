c9cd5efc6b5384d194bcb9fadbdcc3d9449010c1
JoelFaldin <joelfaldin@gmail.com>
2026-05-03 01:26:30 -0400

    feat: add simple tcp server to handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,9 +1,30 @@
 package handler
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
 
 // Each new connection spawns a new gorouting that executes this function.
-func Handler(con net.Conn) {
-	defer con.Close()
+func Handler(conn net.Conn) {
+	defer conn.Close()
 
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		log.Printf("Read error: %v", err)
+		return
+	}
+
+	ackMsg := strings.ToUpper(strings.TrimSpace(message))
+	response := fmt.Sprintf("ACK: %s\n", ackMsg)
+	r, err := conn.Write([]byte(response))
+	if err != nil {
+		log.Printf("Server write error: %v", err)
+	}
+
+	fmt.Println(r)
 }
